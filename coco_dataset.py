c8ee3fef87fda28fa1b8a19d5403566a339c3bdb
Thalles Silva <thalles753@gmail.com>
2022-08-17 15:19:07 -0300

    Add files via upload

---
+++ coco_dataset.py
@@ -0,0 +1,66 @@
+from typing import Any, Callable, List, Optional, Tuple
+import torchvision
+import torch
+
+class CustomCocoCaptions(torchvision.datasets.CocoDetection):
+    """`MS Coco Captions <https://cocodataset.org/#captions-2015>`_ Dataset.
+
+    It requires the `COCO API to be installed <https://github.com/pdollar/coco/tree/master/PythonAPI>`_.
+
+    Args:
+        root (string): Root directory where images are downloaded to.
+        annFile (string): Path to json annotation file.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.PILToTensor``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        transforms (callable, optional): A function/transform that takes input sample and its target as entry
+            and returns a transformed version.
+
+    Example:
+
+        .. code:: python
+
+            import torchvision.datasets as dset
+            import torchvision.transforms as transforms
+            cap = dset.CocoCaptions(root = 'dir where images are',
+                                    annFile = 'json annotation file',
+                                    transform=transforms.PILToTensor())
+
+            print('Number of samples: ', len(cap))
+            img, target = cap[3] # load 4th sample
+
+            print("Image Size: ", img.size())
+            print(target)
+
+        Output: ::
+
+            Number of samples: 82783
+            Image Size: (3L, 427L, 640L)
+            [u'A plane emitting smoke stream flying over a mountain.',
+            u'A plane darts across a bright blue sky behind a mountain covered in snow',
+            u'A plane leaves a contrail above the snowy mountain top.',
+            u'A mountain that has a plane flying overheard in the distance.',
+            u'A mountain view with a plume of smoke in the background']
+
+    """
+    
+    def __init__(
+        self,
+        root: str,
+        annFile: str,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        transforms: Optional[Callable] = None,
+    ) -> None:
+        super().__init__(root, annFile, transform, target_transform, transforms)
+    
+    def _load_target(self, id: int) -> List[str]:
+        
+        ## an image might have more than one description
+        captions = [ann["caption"] for ann in super()._load_target(id)]
+        
+        ## randomly pick one description, transform it to the right representations and return
+        cap_idx = torch.randint(low=0, high=len(captions), size=(1,))
+        
+        return captions[cap_idx]
